44fd29b3831b7236a8c505f06bd6535720d9b0c0
alyza shavers <shavers.alyza@gmail.com>
2023-11-18 09:54:47 -0800

    Contact form created

--- javascript.js
+++ javascript.js
@@ -1,2 +1,22 @@
 // hello world function.
 console.log("Hello world!")
+
+document.addEventListener('DOMContentLoaded', function () {
+    const contactForm = document.getElementById('contactForm');
+    const successMessage = document.getElementById('successMessage');
+  
+    contactForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+  
+      // Simulate form submission and show success message
+      // In a real application, you'd likely send the data to a server using AJAX
+      // and handle the response accordingly.
+      showSuccessMessage();
+    });
+  
+    function showSuccessMessage() {
+      contactForm.classList.add('hidden');
+      successMessage.classList.remove('hidden');
+    }
+  });
+  
